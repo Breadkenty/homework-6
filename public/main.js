0e095e424b8b707faa8bc0603eaa260a6a28dded
Breadkenty <kawakamikento@gmail.com>
2019-04-29 21:02:14 -0400

    additional functions for scoreboard

--- public/main.js
+++ public/main.js
@@ -1,56 +1,135 @@
-score1 = 0;
-score2 = 0;
+let team1Score = 0;
+let team2Score = 0;
 
-const addScore1 = () => {
-  score1 += 1;
-  document.querySelector(".team-1 .score").textContent = score1;
+// Add score
+const team1AddScore = () => {
+  if (team1Score < 21) {
+    team1Score += 1;
+    document.querySelector(".team-1 .score").textContent = team1Score;
+  } else {
+    team1Score += 0;
+  }
+  if (team1Score === 21) {
+    team1Name = document.querySelector(".team-1 .name").textContent;
+    document.querySelector(".dialog h3").textContent = team1Name;
+    document.querySelector("body").className = "body-blur";
+    document.querySelector(".overlay-hidden").className = "overlay-show";
+
+    gameOver();
+  }
+};
+const team2AddScore = () => {
+  if (team2Score < 21) {
+    team2Score += 1;
+    document.querySelector(".team-2 .score").textContent = team2Score;
+  } else {
+    team2Score += 0;
+  }
+  if (team2Score === 21) {
+    team2Name = document.querySelector(".team-2 .name").textContent;
+    document.querySelector(".dialog h3").textContent = team2Name;
+    document.querySelector("body").className = "body-blur";
+    document.querySelector(".overlay-hidden").className = "overlay-show";
+
+    gameOver();
+  }
+};
+// Sub score
+const team1SubScore = () => {
+  if (team1Score > 0) {
+    team1Score -= 1;
+    document.querySelector(".team-1 .score").textContent = team1Score;
+  } else {
+    team1Score -= 0;
+  }
 };
 
-const addScore2 = () => {
-  score2 += 1;
-  document.querySelector(".team-2 .score").textContent = score2;
+const team2SubScore = () => {
+  if (team2Score > 0) {
+    team2Score -= 1;
+    document.querySelector(".team-2 .score").textContent = team2Score;
+  } else {
+    team2Score -= 0;
+  }
 };
 
-const subScore1 = () => {
-  score1 -= 1;
-  document.querySelector(".team-1 .score").textContent = score1;
+// Update Team Name
+const team1UpdateName = () => {
+  team1Name = document.querySelector(".team-1 input").value;
+  document.querySelector(".team-1 .name").textContent = team1Name;
+  document.querySelector(".team-1 input").value = "";
+};
+const team2UpdateName = () => {
+  team2Name = document.querySelector(".team-2 input").value;
+  document.querySelector(".team-2 .name").textContent = team2Name;
+  document.querySelector(".team-2 input").value = "";
 };
 
-const subScore2 = () => {
-  score2 -= 1;
-  document.querySelector(".team-2 .score").textContent = score2;
+// return key
+const team1returnKey = key => {
+  if (key.keyCode === 13) {
+    team1UpdateName();
+  }
 };
 
-const updateName1 = () => {
-  teamName1 = document.querySelector(".team-1 .text-box").value;
-  document.querySelector(".team-1 .name").textContent = teamName1;
-  document.querySelector(".team-1 .text-box").value = "";
+const team2returnKey = key => {
+  if (key.keyCode === 13) {
+    team2UpdateName();
+  }
 };
 
-const updateName2 = () => {
-  teamName2 = document.querySelector(".team-2 .text-box").value;
-  document.querySelector(".team-2 .name").textContent = teamName2;
-  document.querySelector(".team-2 .text-box").value = "";
+// Game Over
+const gameOver = () => {
+  const playAgain = document.querySelector(".dialog button");
+  playAgain.addEventListener("click", resetScore);
+  team1Score = 0;
+  team2Score = 0;
+  document.querySelector(".team-1 .score").textContent = team1Score;
+  document.querySelector(".team-2 .score").textContent = team2Score;
+
+  let buttons = document.querySelectorAll("#disable");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = true;
+  }
+};
+
+const resetScore = () => {
+  document.querySelector("body").className = "";
+  document.querySelector(".overlay-show").className = "overlay-hidden";
+  let buttons = document.querySelectorAll("button");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = false;
+  }
 };
 
 const main = () => {
-  let add1 = document.querySelector(".team-1 .add-1");
-  add1.addEventListener("click", addScore1);
+  // Add Button
+  const team1AddButton = document.querySelector(".team-1 .add-1");
+  team1AddButton.addEventListener("click", team1AddScore);
 
-  let add2 = document.querySelector(".team-2 .add-1");
-  add2.addEventListener("click", addScore2);
+  const team2AddButton = document.querySelector(".team-2 .add-1");
+  team2AddButton.addEventListener("click", team2AddScore);
 
-  let sub1 = document.querySelector(".team-1 .sub-1");
-  sub1.addEventListener("click", subScore1);
+  // Sub Button
+  const team1SubButton = document.querySelector(".team-1 .sub-1");
+  team1SubButton.addEventListener("click", team1SubScore);
 
-  let sub2 = document.querySelector(".team-2 .sub-1");
-  sub2.addEventListener("click", subScore2);
+  const team2SubButton = document.querySelector(".team-2 .sub-1");
+  team2SubButton.addEventListener("click", team2SubScore);
 
-  let updateButton1 = document.querySelector(".team-1 .update-button");
-  updateButton1.addEventListener("click", updateName1);
+  // Update Button
+  const team1UpdateButton = document.querySelector(".team-1 .update-button");
+  team1UpdateButton.addEventListener("click", team1UpdateName);
 
-  let updateButton2 = document.querySelector(".team-2 .update-button");
-  updateButton2.addEventListener("click", updateName2);
+  const team2UpdateButton = document.querySelector(".team-2 .update-button");
+  team2UpdateButton.addEventListener("click", team2UpdateName);
+
+  // Press 'return' to Update Name
+  const team1ReturnKey = document.querySelector(".team-1 .text-box");
+  team1ReturnKey.addEventListener("keydown", team1returnKey);
+
+  const team2ReturnKey = document.querySelector(".team-2 .text-box");
+  team2ReturnKey.addEventListener("keydown", team2returnKey);
 };
 
 document.addEventListener("DOMContentLoaded", main);
